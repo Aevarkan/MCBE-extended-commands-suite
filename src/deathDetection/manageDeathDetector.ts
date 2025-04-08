5d8bca6e7026c2a9de520727dddf7b40cff3bf9d
Aevarkan <aevarkan@gmail.com>
2025-04-08 15:58:15 +0800

    spelling

--- src/deathDetection/manageDeathDetector.ts
+++ src/deathDetection/manageDeathDetector.ts
@@ -1,7 +1,7 @@
 /**
  * This file is part of Extended Commands Suite which is released under GPL-3.0.
  * See file LICENCE or go to https://www.gnu.org/licenses/gpl-3.0.en.html for full license details.
- * File: manageDeathDetection.ts
+ * File: manageDeathDetector.ts
  * Author: Aevarkan
  */
 
