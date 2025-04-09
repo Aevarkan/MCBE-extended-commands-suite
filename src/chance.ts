6e24ed8ff1a68e4dffd3d574c6cf48515b07191b
Aevarkan <aevarkan@gmail.com>
2025-04-10 00:12:34 +0800

    fixed chance command

--- src/chance.ts
+++ src/chance.ts
@@ -5,19 +5,41 @@
  * Author: Aevarkan
  */
 
-import { Entity, ScriptEventCommandMessageAfterEvent, system } from "@minecraft/server";
+import { Dimension, Entity, ScriptEventCommandMessageAfterEvent, system, world } from "@minecraft/server";
 
 export function chance(event: ScriptEventCommandMessageAfterEvent) {
     const entity = event.sourceEntity
+    const block = event.sourceBlock
+    const blockDimension = block.dimension
     const parameters = event.message
 
     // Split the message into command and chance
     const parts = parameters.split(" ")
     const percentageChance = parseInt(parts[0])
-    const command = parts.slice().join(" ")
+    const command = parts.slice(1).join(" ")
 
     // Executes the command
-    chanceAction(entity, command, percentageChance)
+    if (entity) chanceActionEntity(entity, command, percentageChance)
+    if (block) chanceActionBlock(blockDimension, command, percentageChance)
+}
+
+/**
+ * Makes the world sometimes run a command.
+ * @param block The block that called the command.
+ * @param command The command string.
+ * @param percentageChance How likely the world is to run the command.
+ */
+function chanceActionBlock(dimension: Dimension, command: string, percentageChance: number) {
+    
+    // Number between 0 and 100
+    const runChance = Math.random() * 100
+
+    // Run the command if number is higher than given chance
+    if (runChance < percentageChance) {
+        system.run(() => {
+            dimension.runCommand(command)
+        })
+    }
 }
 
 /**
@@ -26,7 +48,7 @@ export function chance(event: ScriptEventCommandMessageAfterEvent) {
  * @param command The command string.
  * @param percentageChance How likely the entity is to run the command.
  */
-function chanceAction(entity: Entity, command: string, percentageChance: number) {
+function chanceActionEntity(entity: Entity, command: string, percentageChance: number) {
     
     // Number between 0 and 100
     const runChance = Math.random() * 100
