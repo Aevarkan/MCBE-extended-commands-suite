5ce716cb24fd4f2d9974d1d351b83d02197f4daa
Aevarkan <aevarkan@gmail.com>
2025-04-17 15:33:25 +0800

    webhook test

--- src/projectile.ts
+++ src/projectile.ts
@@ -13,16 +13,6 @@ import { DimensionLocation, Entity, EntityComponentTypes, ScriptEventCommandMess
 // /projectile will let you spawn a projectile at any location
 // /shoot only from an entity
 
-
-// Testing notes
-// Only works with arrows, wind charges
-// Only things that appear in /summon work
-
-// TODO: See if custom entity can do it:
-// minecraft:behavior.ranged_attack
-// minecraft:shooter
-// OR, this is like the /scale command, we need to edit the vanilla files
-// change is_summonable to true, have to warn users about this however!
 export function shoot(event: ScriptEventCommandMessageAfterEvent) {
     const entity = event.sourceEntity
     const parameters = event.message
