df53adc067c6a6a2b23ccab7dbb3af4db655ead3
Aevarkan <aevarkan@gmail.com>
2025-05-03 22:35:35 +0800

    fixed file name in header

--- src/server/CommandDetections/jumpDetection/manageJumpDetector.ts
+++ src/server/CommandDetections/jumpDetection/manageJumpDetector.ts
@@ -1,7 +1,7 @@
 /**
  * This file is part of Extended Commands Suite which is released under GPL-3.0.
  * See file LICENCE or go to https://www.gnu.org/licenses/gpl-3.0.en.html for full licence details.
- * File: manageEmoteDetector.ts
+ * File: managePunchDetector.ts
  * Author: Aevarkan
  */
 
