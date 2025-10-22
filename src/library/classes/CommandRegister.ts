6a44e46c1309914579a79c549278da912b0b2cc5
Aevarkan <aevarkan@gmail.com>
2025-10-22 18:51:55 +0800

    feat: add custom command handler class

---
+++ src/library/classes/CommandRegister.ts
@@ -0,0 +1,75 @@
+/**
+ * This file is part of Areas which is released under GPL-3.0.
+ * See file LICENCE or go to https://www.gnu.org/licenses/gpl-3.0.en.html for full licence details.
+ * File: CommandRegister.ts
+ * Author: Aevarkan
+ */
+
+import { CustomCommand, CustomCommandOrigin, CustomCommandResult, CustomCommandStatus, system } from "@minecraft/server";
+import { CommandInfo } from "./types/customCommands";
+import config from "config";
+
+export class CommandRegister {
+
+    private static commandsToRegister: CommandInfo[] = []
+
+    /**
+     * Register a new command.
+     * @param commandInfo The command info object.
+     */
+    public static registerCommand(commandInfo: CommandInfo) {
+
+        this.commandsToRegister.push(commandInfo)
+    }
+
+    // public static getCommands() {
+    //     return this.commandsToRegister
+    // }
+
+
+    /**
+     * Registers all custom commands.
+     * 
+     * @internal
+     * This should only be run in main after all commands are registered.
+     */
+    public static _registerCommands() {
+        system.beforeEvents.startup.subscribe(event => {
+            const commandRegistry = event.customCommandRegistry
+
+            // Register each command put in the register
+            this.commandsToRegister.forEach(command => {
+
+                const namespacedName = config.commandPrefix + ":" + command.name
+
+                const customCommand: CustomCommand = {
+                    name: namespacedName,
+                    description: command.description,
+                    permissionLevel: command.permissionLevel,
+                    cheatsRequired: command.cheatsRequired,
+                    mandatoryParameters: command.mandatoryParameters,
+                    optionalParameters: command.optionalParameters
+                }
+
+                function callbackWrapper(origin: CustomCommandOrigin, ...args: any[]): CustomCommandResult {
+                    try {
+                        const result = command.callbackFunction(origin, ...args)
+
+                        // If the command returns false, then it's also a failure (instead of throwing an error)
+                        if (result === false) {
+                            return { message: command.failureMessage, status: CustomCommandStatus.Failure }
+                        } else {
+                            return { message: command.successMessage, status: CustomCommandStatus.Success }
+                        }
+                    } catch (error) {
+                        return { message: command.failureMessage, status: CustomCommandStatus.Failure }
+                    }
+                }
+
+                commandRegistry.registerCommand(customCommand, callbackWrapper)
+            })
+            
+        })
+    }
+
+}
---
+++ src/library/classes/types/customCommands.ts
@@ -0,0 +1,65 @@
+/**
+ * This file is part of Areas which is released under GPL-3.0.
+ * See file LICENCE or go to https://www.gnu.org/licenses/gpl-3.0.en.html for full licence details.
+ * File: customCommands.ts
+ * Author: Aevarkan
+ */
+
+import { CommandPermissionLevel, CustomCommandOrigin, CustomCommandParameter } from "@minecraft/server";
+
+export interface CommandInfo {
+    /**
+     * The callback function the command should run.
+     * @remarks This function should throw an error or return false to indicate command failure.
+     * 
+     */
+    callbackFunction: (origin: CustomCommandOrigin, ...args: any[]) => void | boolean
+    /**
+     * @remarks
+     * Message displayed to chat after successful command execution.
+     *
+     */
+    successMessage?: string
+    /**
+     * @remarks
+     * Message displayed to chat after unsuccessful command execution.
+     *
+     */
+    failureMessage?: string
+    /**
+     * @remarks
+     * Whether or not cheats must be enabled to run the command. Defaults to true.
+     * 
+     */
+    cheatsRequired?: boolean
+    /**
+     * @remarks
+     * The name of the command. The namespace will be added automatically.
+     *
+     */
+    name: string
+    /**
+     * @remarks
+     * Command description as seen on the command line.
+     *
+     */
+    description: string
+    /**
+     * @remarks
+     * The permission level required to execute the command.
+     *
+     */
+    permissionLevel: CommandPermissionLevel
+    /**
+     * @remarks
+     * List of mandatory command parameters.
+     *
+     */
+    mandatoryParameters?: CustomCommandParameter[]
+    /**
+     * @remarks
+     * List of optional command parameters.
+     *
+     */
+    optionalParameters?: CustomCommandParameter[]
+}
