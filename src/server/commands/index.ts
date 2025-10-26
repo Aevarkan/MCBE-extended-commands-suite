886d03717c781cfbb773a905cd18019d5dcd6460
Aevarkan <aevarkan@gmail.com>
2025-10-26 12:53:20 +0800

    chore: add placeholder imports in command index

--- src/server/commands/index.ts
+++ src/server/commands/index.ts
@@ -7,5 +7,15 @@
 
 import "./chance"
 import "./drop"
-import "./entityLock"
+import "./entityLock" // TODO: needs an update
+// import "./freeze"
+// import "./keepInventory"
+// import "./manageMusic"
+// multicommand is not getting updated
+// import "./projectile"
+// import "./push" // NOTE: motion and push will be separate behaviours
+// import "./schedule"
 import "./setHealth"
+// import "./smite"
+// import "./termin'"
+// import "./tpSpawn"
