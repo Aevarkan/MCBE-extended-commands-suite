262a4c4cd44e1ee0cfa1aa93dae607ff8d9ab59e
Aevarkan <aevarkan@gmail.com>
2025-11-02 15:02:37 +0800

    chore: version bump
    using `0.99` as `-beta` tag might not work

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ import zipfile
 
 # No need to include the 'v' in front
 # Only change version count here and as github tag
-VERSION = "1.0.0"
+VERSION = "0.99.0"
 MIN_ENGINE_VERSION = [ 1, 21, 111 ]
 
 CONSTANTS_TS_PATH = "src/constants.ts"
